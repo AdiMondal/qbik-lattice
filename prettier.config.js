0d857745eca881dc676fda3877b8c824c370979b
AdiMondal <144584127+AdiMondal@users.noreply.github.com>
2024-02-15 19:47:03 +0530

    configuration aof the project done!

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const options = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    bracketSpacing: true,
+    endofLine: 'lf',
+    semi: false,
+    tabWidth: 2,
+    trailingComma: 'none'
+}
+export default options;
